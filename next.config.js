a3a60fe6a58033e22f06df1f876852b42ce37bbf
Mohcka <MohckaMas@proton.me>
2023-08-19 16:49:56 -0400

    adds image optimization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "commondatastorage.googleapis.com",
+      "uhdtv.io",
+      "download.blender.org",
+      "mango.blender.org",
+      "i.ytimg.com",
+      "www.blender.org",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
