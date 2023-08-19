a172e74af1e5115ee05573d4ca85d00943a7cd71
Mohcka <MohckaMas@proton.me>
2023-08-19 12:45:17 -0400

    Reformatting...

--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -59,7 +59,7 @@ export default function Profiles() {
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
         <h1 className="text-3xl md:text-6xl text-white text-center">
-          Who&#39;s watching?
+          {"Who's watching?"}
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10">
           <div onClick={() => selectProfile()}>
